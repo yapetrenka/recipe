ea7beeaccb417a7af144d58d807134dd0be2faaf
yapetrenka <yapetrenka@yandex.ru>
2024-12-05 12:03:51 +0300

    базовый список рецептов

---
+++ admin/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9aa5a6b2-29f1-43c8-89a1-35184bc9d438'
+};
---
+++ frontend/src/components/RecipeList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Рецепты</h1>
+    <ul>
+      <li v-for="recipe in recipes" :key="recipe.id">
+        <h2>{{ recipe.title }}</h2>
+        <p>{{ recipe.description }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'RecipeList',
+  data() {
+    return {
+      recipes: []
+    };
+  },
+  created() {
+    axios.get('http://localhost:1337/recipes')
+        .then(response => {
+          this.recipes = response.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+  }
+};
+</script>
