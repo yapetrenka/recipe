bdd7ead56f626111a917f8373bded1a2109accec
yapetrenka <yapetrenka@yandex.ru>
2024-12-10 16:52:57 +0300

    вернула все как было

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,6 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 export default defineConfig({
-    root: 'frontend',
-    build: {
-        outDir: 'dist'
-    },
     plugins: [vue()],
     server: {
         port: 3002
