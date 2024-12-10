996254ec6769b6f91985a8e1b5413cfbd2b2e75a
yapetrenka <yapetrenka@yandex.ru>
2024-12-10 16:16:08 +0300

    бновлена конфигурация Vite для указания корневой и выходной директории

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,10 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 export default defineConfig({
+    root: 'frontend',
+    build: {
+        outDir: 'dist'
+    },
     plugins: [vue()],
     server: {
         port: 3002
