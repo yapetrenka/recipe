bc3e9e94d5a7186469cbb6d2f4be893f0184f29e
yapetrenka <yapetrenka@yandex.ru>
2024-12-10 16:28:45 +0300

    настройка директории dist

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import path from 'path';
 export default defineConfig({
     root: 'frontend',
     build: {
-        outDir: 'dist'
+        outDir: 'frontend/dist'
     },
     plugins: [vue()],
     server: {
